97ac628116f9d1fdfab81bcecbd730624d6f4911
Ethan Daniel <ethandaniel@berkeley.edu>
2020-01-28 19:46:30 -0800

    deleted config with password

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-WTF_CSRF_ENABLED = True
-SECRET_KEY = 'you-will-never-guess'
-ROOT_PATH = os.path.join(basedir, "app")
-NUM_PREVIEW_CHARS = 150
-class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
